6b54a8af4c1d517b572e52ea6e7fdacfd725ba87
vinay kumar <vinay.kumar@getvymo.com>
2021-10-25 12:48:46 +0530

    intial setup of the project

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
